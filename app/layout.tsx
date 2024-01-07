46b96a9ca876de20bc2de130a9fb59e0da809a1c
nurfaisall <nurfaisal1504@gmail.com>
2024-01-07 22:39:35 +0700

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+import logo from '@/public/assets/logo.svg'
+import './globals.css'
+import { ClerkProvider } from '@clerk/nextjs'
+
+const poppins = Poppins({ subsets: ['latin'],
+weight: ['400', '500', '600', '700'],
+variable: '--font-poppins' })
+
+export const metadata: Metadata = {
+  title: 'Evently',
+  description: 'Evently is platform for event management',
+  icons: {
+    icon : logo 
+  }
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+  )
+}
